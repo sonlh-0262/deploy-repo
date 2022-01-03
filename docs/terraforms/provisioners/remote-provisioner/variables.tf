04d8cb34a714b9d18c2226769e49b12adb5b43f9
jameson <49262845+sonlh-0262@users.noreply.github.com>
2022-01-03 23:44:47 +0700

    Create variables.tf

---
+++ docs/terraforms/provisioners/remote-provisioner/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type = string 
+}
+
+variable "availability_zone" {
+  type = string 
+}
+
+variable "req_public_ip" {
+  type = bool 
+}
+
+variable "instance_type" {
+  type = string 
+}
+
+variable "aws_instance_name" {
+  type = string 
+}
