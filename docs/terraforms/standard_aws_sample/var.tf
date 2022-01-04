ac47b78cb124df5470643e82d9871cc7a82c0b68
jameson <49262845+sonlh-0262@users.noreply.github.com>
2022-01-04 13:55:23 +0700

    Update var.tf

--- docs/terraforms/standard_aws_sample/var.tf
+++ docs/terraforms/standard_aws_sample/var.tf
@@ -1,1 +1,19 @@
+variable "aws_region" {
+  type = string  
+}
 
+variable "aws_public_azs" {
+  type = string 
+}
+
+variable "aws_cidr_mgmt" {
+   
+}
+
+variable "secGroupName" {
+  type = string 
+}
+
+variable "secGroupDescription" {
+  type = string 
+}
