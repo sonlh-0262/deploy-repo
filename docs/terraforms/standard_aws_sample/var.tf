73e1bf6e4162b6e920592add275773b82e292b2e
jameson <49262845+sonlh-0262@users.noreply.github.com>
2022-01-04 14:03:29 +0700

    Update var.tf

--- docs/terraforms/standard_aws_sample/var.tf
+++ docs/terraforms/standard_aws_sample/var.tf
@@ -17,3 +17,28 @@ variable "secGroupName" {
 variable "secGroupDescription" {
   type = string 
 }
+
+variable "vpc_cidr" {
+  type = string
+  default = "10.211.0.0/16"
+}
+
+variable "vpc_name" {
+  type = string
+  default = "demo-vpc"
+}
+
+variable "internet_gw_name" {
+  type = string
+  default = "demo-ig"
+}
+
+variable "public_rt_name" {
+  type = string
+  default = "demo-public-route"
+}
+
+variable "subnet_name" {
+  type = string
+  default = "demo-mgmt-subnet"
+}
